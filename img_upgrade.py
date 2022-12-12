ddfb9533b83e810e7c597c8c4de0f1ad63183df6
NicMilli <91165462+NicMilli@users.noreply.github.com>
2022-12-12 05:10:59 -0800

    Image transformation finished

--- img_upgrade.py
+++ img_upgrade.py
@@ -5,6 +5,8 @@ import cv2
 from matplotlib import pyplot as plt
 import argparse
 import imutils
+from skimage.filters import threshold_local
+import datetime
 
 # #Argument parser for command line use -- to be added later
 # ap = argparse.ArgumentParser()
@@ -28,31 +30,32 @@ median = cv2.GaussianBlur(gray, (5, 5), 0)
 img_bw = cv2.threshold(median, thresh, 255, cv2.THRESH_BINARY)[1]
 img_edged = cv2.Canny(gray, 75, 200)
 
-#Laplacian and Sobel transformations
-lap = cv2.Laplacian(img_bw, cv2.CV_64F, ksize = 1)
-lap = np.uint8(np.absolute(lap))
+##Other image processing methods, not used!
+# #Laplacian and Sobel transformations
+# lap = cv2.Laplacian(img_bw, cv2.CV_64F, ksize = 1)
+# lap = np.uint8(np.absolute(lap))
 
-sobelX = cv2.Sobel(img_bw, cv2.CV_64F, 1, 0)
-sobelY = cv2.Sobel(img_bw, cv2.CV_64F, 0, 1)
+# sobelX = cv2.Sobel(img_bw, cv2.CV_64F, 1, 0)
+# sobelY = cv2.Sobel(img_bw, cv2.CV_64F, 0, 1)
 
-sobelX = np.uint8(np.absolute(sobelX))
-sobelY = np.uint8(np.absolute(sobelY))
+# sobelX = np.uint8(np.absolute(sobelX))
+# sobelY = np.uint8(np.absolute(sobelY))
 
-sobelXY = cv2.bitwise_or(sobelX, sobelY)
+# sobelXY = cv2.bitwise_or(sobelX, sobelY)
 
-#Print results for comparison
-titles = ['image', 'Laplacian', 'sobelXY', 'edged']
-images = [img_bw, lap, sobelXY, img_edged]
+# #Print results for comparison
+# titles = ['image', 'Laplacian', 'sobelXY', 'edged']
+# images = [img_bw, lap, sobelXY, img_edged]
 
-fig = plt.figure()
+# fig = plt.figure()
 
-for num, img in enumerate(images):
-    plt.subplot(2, 2, num+1)
-    plt.title(titles[num])
-    plt.axis('off')
-    plt.imshow(img, 'gray')
+# for num, img in enumerate(images):
+#     plt.subplot(2, 2, num+1)
+#     plt.title(titles[num])
+#     plt.axis('off')
+#     plt.imshow(img, 'gray')
 
-plt.show()
+# plt.show()
 
 #Detect the edges 
 # - using assumption that receipt will be main focus of image and have 4 points
@@ -67,7 +70,71 @@ for index, contour in enumerate(contours):
     if len(approximate) == 4:
         screenF = approximate
         break
+  
+##Functions to organize points for cv.getPerspectiveTransform  
+def order_corners(points):
+    #Order = top left, top right, bottom left, bottom right#
+    rectangle = np.zeros((4, 2), dtype = "float32")
     
-def get_corners(pts):
+    ##Require receipt photo input long ways 
+    #top left = smallest sum, bottom right = largest
+    xy_sum = points.sum(axis = 1)
+    rectangle[2] = points[np.argmax(xy_sum)]
+    rectangle[0] = points[np.argmin(xy_sum)]
     
-    rectangle = np.zeros(())
+    #top right = smallest diff; bottom left = largest diff
+    xy_diff = np.diff(points, axis = 1)
+    rectangle[1] = points[np.argmin(xy_diff)]
+    rectangle[3] = points[np.argmax(xy_diff)]
+    
+    return rectangle
+
+def transform(image, points):
+    #order and define the corners
+    rectangle = order_corners(points)
+    (top_left, top_right, bottom_right, bottom_left) = rectangle
+    
+    #to find the width of the corrected image
+    #find maximum width between top points or bottom points
+    width_bottom = np.sqrt(((bottom_right[0] - bottom_left[0]) ** 2) + ((bottom_right[1] - bottom_left[1]) ** 2))
+    width_top = np.sqrt(((top_right[0] - top_left[0]) ** 2) + ((top_right[1] - top_left[1]) ** 2))
+    fixed_width = max(int(width_bottom), int(width_top))
+    
+    #Similaryl find height from max of left points height and right points height
+    height_left = np.sqrt(((top_left[0] - bottom_left[0]) ** 2) + ((top_left[1] - bottom_left[1]) ** 2))
+    height_right = np.sqrt(((top_right[0] - bottom_right[0]) ** 2) + ((top_right[1] - bottom_right[1]) ** 2))
+    fixed_height = max(int(height_left), int(height_right))
+    
+    destination_points = np.array([[0, 0], [fixed_width - 1, 0],
+                                   [fixed_width - 1, fixed_height - 1], [0, fixed_height - 1]],
+                                  dtype = "float32")
+    transformation_matrix = cv2.getPerspectiveTransform(rectangle, destination_points)
+    fixed_image = cv2.warpPerspective(image, transformation_matrix, (fixed_width, fixed_height))
+    
+    return fixed_image
+
+fixed_image = transform(original, screenF.reshape(4, 2) * ratio)
+
+start1 = datetime.datetime.now()
+thresh = 127
+gray = cv2.cvtColor(fixed_image, cv2.COLOR_BGR2GRAY)
+median = cv2.GaussianBlur(gray, (5, 5), 0)
+img_bw = cv2.threshold(median, thresh, 255, cv2.THRESH_BINARY)[1]
+end1 = datetime.datetime.now()
+
+start2 = datetime.datetime.now()
+fixed_image = cv2.cvtColor(fixed_image, cv2.COLOR_BGR2GRAY)
+T = threshold_local(fixed_image, 11, offset = 10, method = "gaussian")
+fixed_image = (fixed_image > T).astype("uint8") * 255
+end2 = datetime.datetime.now()
+
+rt1 = end1 - start1
+rt2 = end2 - start2
+print(rt1)
+print(rt2)
+# show the original and scanned images
+print("STEP 3: Apply perspective transform")
+cv2.imshow("Original", imutils.resize(original, height = 650))
+cv2.imshow("Scanned1", imutils.resize(img_bw, height = 650))
+cv2.imshow("Scanned2", imutils.resize(fixed_image, height = 650))
+cv2.waitKey(0)
